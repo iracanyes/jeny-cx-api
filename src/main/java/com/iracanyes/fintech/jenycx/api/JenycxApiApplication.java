702d38598ed385667696a88488afa80d88c0006e
iracanyes <iracanyes@gmail.com>
2024-05-19 08:43:04 +0200

    Init

---
+++ src/main/java/com/iracanyes/fintech/jenycx/api/JenycxApiApplication.java
@@ -0,0 +1,13 @@
+package com.iracanyes.fintech.jenycx.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenycxApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JenycxApiApplication.class, args);
+  }
+
+}
